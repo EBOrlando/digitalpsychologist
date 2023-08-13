303b242bb3d72f9845444b712991973f7983dd39
EBOrlando <eduardo.barreraorlando@gmail.com>
2023-08-13 14:59:41 -0300

    Update digitalpsychologist.py
    pequenos ajustes

--- digitalpsychologist.py
+++ digitalpsychologist.py
@@ -19,22 +19,22 @@ def CustomChatGPT(user_input):
         if "quota" in str(e).lower():
             return "Seu saldo é insuficiente para continuar usando a ferramenta. Por favor atualize suas opções de pagamento."
         else:
-            return "An error occurred: " + str(e)
+            return "Um erro aconteceu: " + str(e)
 
 def main():
-    st.title("Digital Psychologist")
+    st.title("Seu Assistente Emocional Pessoal")
 
-    st.write("Welcome! Please enter your OpenAI API key to begin the session.")
+    st.write("Bem vindo! Insira sua OpenAI key para continuar com a sessão.")
     api_key = st.text_input("API Key", type="password")
 
     if api_key:
         openai.api_key = api_key  # Set the OpenAI API key
-        st.write("You can now chat with the digital psychologist.")
-        user_input = st.text_input("You:")
+        st.write("Você agora pode conversar com seu Assistente Emocional Pessoal.")
+        user_input = st.text_input("Você:")
         
         if user_input:
             response = CustomChatGPT(user_input)
-            st.text_area("Psychologist:", value=response, height=200, max_chars=None)
+            st.text_area("Assistente:", value=response, height=200, max_chars=None)
 
 if __name__ == "__main__":
     main()
